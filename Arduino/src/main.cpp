17a7d04855e323cb1f8f4c34385666db450c52a0
Henri van de Munt <henrivandemunt@gmail.com>
2017-05-08 17:22:13 +0200

    Car now driving on keyboard via bluetooth

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -1,21 +1,24 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
-int pwmDrive = 5;    // pwm drive
-int drive = 4;
-int pwmDirection = 6;          // pwm direction
-int direction = 7;
+#define PWMDRIVE 4
+#define PWMSTEER 5
+#define BACKWARDS 6
+#define FORWARDS 7
+#define LEFT 8
+#define RIGHT 9
+
+#define TRIGPINR 13 // right seen from driver position
+#define ECHOPINR 12
+#define TRIGPINL 11 // left seen from driver position
+#define ECHOPINL 10
+
 
 SoftwareSerial BTserial(2, 3); // RX | TX
 // Connect the HC-05 TX to Arduino pin 2 RX.
 // Connect the HC-05 RX to Arduino pin 3 TX through a voltage divider.
 
 char receivedData = ' ';
-
-char speed = 100;
-unsigned int timeSetDirection;
-unsigned int timeSetDrive;
-unsigned int currentSpeed;
 
 void forwards();
 void backwards();
@@ -25,95 +28,99 @@ void right();
 void hold();
 
 void setup() {
-  pinMode(drive, OUTPUT);
-  pinMode(direction, OUTPUT);
   BTserial.begin(115200);
   Serial.begin(9600);
-  timeSetDirection = millis();
-  timeSetDrive = millis();
-  currentSpeed = 0;
+
+  // engine control
+  analogWrite(PWMDRIVE, 150);   // PWM Speed Control
+  analogWrite(PWMSTEER, 255);   // PWM Steer Control
+
+  // sensor Control
+  pinMode(TRIGPINL, OUTPUT);
+  pinMode(ECHOPINL, INPUT);
+  pinMode(TRIGPINR, OUTPUT);
+  pinMode(ECHOPINR, INPUT);
 }
 
 void loop(){
+  while (1) {
+      long duration, distance;
+      digitalWrite(TRIGPINR, LOW); // Added this line
+      delayMicroseconds(2); // Added this line
+      digitalWrite(TRIGPINR, HIGH);
+//  delayMicroseconds(1000); - Removed this line
+      delayMicroseconds(10); // Added this line
+      digitalWrite(TRIGPINR, LOW);
+      duration = pulseIn(ECHOPINR, HIGH);
+      distance = (duration/2) / 29.1;
+      if (distance >= 450 || distance <= 0) {
+          Serial.println("Out of range");
+        }else {
+          Serial.print(distance);
+          Serial.println(" cm");
+        }
+      delay(100);
+    }
+
   while (1) {
       if (BTserial.available()) {
           receivedData = BTserial.read();
           Serial.write(receivedData);
           switch (receivedData) {
-            case 'c':
+            case 'W':
               forwards();
               break;
-            case 'a':
+            case 'A':
               left();
               break;
-            case 'd':
+            case 'S':
               backwards();
               break;
-            case 'b':
+            case 'D':
               right();
               break;
-            case 'f':   // hold
+            case 'H':   // hold
               hold();
               break;
-            case 's':
-              if (BTserial.available()) {
-                  speed = BTserial.read();
-                  Serial.println(speed);
-                }
+            case 'C':     // hold
+              center();
+              break;
+            default:
+              // hold();
+              // if nothing else match, do the default
+              // default is optional
               break;
             }
         }
-
-      if (timeSetDirection + 250 <= millis()) {
-          center();
-        }
-      if (timeSetDrive + 250 <= millis()) {
-          hold();
-        }
-
     }
 }
+
 void forwards(){
-  timeSetDrive = millis();
-  digitalWrite(drive, LOW);   // LOW forwards, HIGH backwards
-  // start engine at low speed increase to higer speed
-  for (char i = currentSpeed; i < speed; i++) {
-      analogWrite(pwmDrive, i);
-      delay(10/i);
-    }
-  analogWrite(pwmDrive, speed);   // PWM Speed Control
-  currentSpeed = speed;
+  digitalWrite(FORWARDS, HIGH);
+  digitalWrite(BACKWARDS, LOW);
 }
 
 void backwards(){
-  timeSetDrive = millis();
-  digitalWrite(drive, HIGH);   // LOW forwards, HIGH backwards
-  // start engine at low speed increase to higer speed
-  for (char i = currentSpeed; i < speed; i++) {
-      analogWrite(pwmDrive, i);
-      delay(10/i);
-    }
-  analogWrite(pwmDrive, speed);   // PWM Speed Control
-  currentSpeed = speed;
+  digitalWrite(FORWARDS, LOW);
+  digitalWrite(BACKWARDS, HIGH);
 }
 
 void left(){
-  timeSetDirection = millis();
-  digitalWrite(direction, HIGH); // LOW right, HIGH left
-  analogWrite(pwmDirection, 255); // PWM Speed Control
+  digitalWrite(LEFT, HIGH);
+  digitalWrite(RIGHT, LOW);
 }
 
 void center(){
-  analogWrite(pwmDirection, 0);        // PWM Speed Control
+  digitalWrite(LEFT, LOW);
+  digitalWrite(RIGHT, LOW);
 }
 
 void right(){
-  timeSetDirection = millis();
-  digitalWrite(direction, LOW);   // LOW right, HIGH left
-  analogWrite(pwmDirection, 255);       // PWM Speed Control
+  digitalWrite(RIGHT, HIGH);
+  digitalWrite(LEFT, LOW);
 }
 
 void hold(){
-  analogWrite(pwmDrive, 0);   // PWM Speed Control
-  currentSpeed = 0;
+  digitalWrite(FORWARDS, LOW);
+  digitalWrite(BACKWARDS, LOW);
 }
